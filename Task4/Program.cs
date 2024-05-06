a3335450091d8bec4e1a7b30135ec4549a94d398
Raymiray <ritsiscream11@yandex.ru>
2024-05-06 19:34:59 +0300

    add homework:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Enter a natural number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 0)
+{
+   if (n < 10)
+   {
+        Console.WriteLine(n);
+   }
+   else
+   {
+    while (n > 0)
+    {
+        int currentDigit = n % 10;
+        n /= 10;
+        if (n > 0)
+        {
+            Console.Write(currentDigit + ",");
+        }
+        else
+        {
+            Console.WriteLine(currentDigit);
+        }
+    }
+   }
+}
+else
+{
+    Console.WriteLine("Erorr");
+}

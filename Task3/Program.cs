aca77632a86ffa891c7bd8cf0b101051611419a2
Raymiray <ritsiscream11@yandex.ru>
2024-05-06 18:49:20 +0300

    add homework:task3

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.WriteLine("Enter a positive two-digit whole number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10 && num <= 99)
+{
+    int firstDigit = num / 10;
+    int lastDigit = num % 10;
+
+    if (firstDigit > lastDigit)
+    {
+        Console.WriteLine(firstDigit);        
+    }
+    else
+    {
+        Console.WriteLine(lastDigit);
+    }
+    
+}
+else
+{
+    Console.WriteLine("Erorr");
+}
